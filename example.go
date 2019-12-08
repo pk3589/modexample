1e81446d768d18abfe2f062d26cce6a4c59f05d2
TigerLee <pk3589@163.com>
2019-12-08 18:26:06 +0800

    add routergroup code

--- example.go
+++ example.go
@@ -11,6 +11,17 @@ func main() {
 
 	r := gin.Default()
 
+	v1 := r.Group("/v1")
+
+	{
+		v1.GET("/ping",func(c *gin.Context){
+			c.JSON(200, gin.H{
+
+				"message": " v1 pong",
+			})
+		})
+	}
+
 	r.GET("/ping", func(c *gin.Context) {
 
 		c.JSON(200, gin.H{
